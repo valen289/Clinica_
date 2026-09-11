a4a947d0676c8573c2aa0a41bf0fc5f55a7f0cdb
valen289 <valeencoria28@gmail.com>
2026-09-11 08:39:56 -0300

    configuracion de config.php para conectar con el servidor

--- config/config.php
+++ config/config.php
@@ -1,7 +1,9 @@
 <?php
 
-define('BD_HOST', 'localhost');
-define('BD_USUARIO', 'root');
-define('BD_CLAVE', '');
-define('BD_NOMBRE', 'hospital_clinicas');
-define('BASE_URL', '/Clinica_/');
+// Los valores por defecto son para desarrollo local; en el servidor se
+// sobrescriben seteando estas mismas variables de entorno (sin tocar este archivo).
+define('BD_HOST', getenv('BD_HOST') ?: 'localhost');
+define('BD_USUARIO', getenv('BD_USUARIO') ?: 'root');
+define('BD_CLAVE', getenv('BD_CLAVE') ?: '');
+define('BD_NOMBRE', getenv('BD_NOMBRE') ?: 'hospital_clinicas');
+define('BASE_URL', getenv('BASE_URL') ?: '/Clinica_/');
